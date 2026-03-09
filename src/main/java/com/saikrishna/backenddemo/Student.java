669237058c7f0ea2b0ef3930cf31bf4d743806d7
Sai Krishna Tangudu <saikrishnatangudu1310@gmail.com>
2026-03-09 12:13:08 +0530

    Add student ID support and GET /students/{id} endpoint

--- src/main/java/com/saikrishna/backenddemo/Student.java
+++ src/main/java/com/saikrishna/backenddemo/Student.java
@@ -2,18 +2,22 @@ package com.saikrishna.backenddemo;
 
 public class Student
 {
+    private int id;
     private String name;
     private int age;
     private String department;
 
     public Student(){}
 
-    public Student(String name, int age , String department)
+    public Student(int id, String name, int age , String department)
     {
+        this.id = id;
         this.name = name;
         this.age = age;
         this.department = department;
     }
+
+    public int getId(){ return id; }
 
     public String getName()
     {
--- src/main/java/com/saikrishna/backenddemo/controller/HelloController.java
+++ src/main/java/com/saikrishna/backenddemo/controller/HelloController.java
@@ -4,33 +4,41 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.ArrayList;
 
 @RestController
 public class HelloController {
 
-    @GetMapping("/students")
-    public List<Student> getStudents()
-    {
-        Student s1 = new Student("Sai Krishna",19,"CSE Cyber Securty");
-        Student s2 = new Student("Ayush Kumar",20,"IT");
-        Student s3 = new Student("Teja Abhinay", 19, "CSE");
+    private List<Student> students = new ArrayList<>();
 
-        return List.of(s1, s2, s3);
+    @GetMapping("/students")
+    public List<Student> getStudent()
+    {
+        return students;
     }
-    @GetMapping("/student")
-    public Student getStudent(){
-        return new Student("Sai Krishna",20,"Computer Science Engineering");
-        }
-    @PostMapping("/student")
+
+    @PostMapping("/students")
     public Student createStudent(@RequestBody Student student)
     {
+        students.add(student);
         return student;
     }
-    @GetMapping("/wish")
-    public String sayHello() {
-        return "hello";
+
+    @GetMapping("/students/{id}")
+    public Student getStudentById(@PathVariable int id)
+    {
+        for(Student s: students)
+        {
+            if(s.getId() == id)
+            {
+                return s;
+            }
+        }
+        return null;
     }
+
 
 }
